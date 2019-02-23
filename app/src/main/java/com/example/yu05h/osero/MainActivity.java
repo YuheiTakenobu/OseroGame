4ff0991b1b8fd6e142b7b6f9e6961576f017e5ae
YuheiTakenobu <b1016127@fun.ac.jp>
2019-02-24 05:59:37 +0900

    ㊇8×8のマス目をボタンで実装

--- app/src/main/java/com/example/yu05h/osero/MainActivity.java
+++ app/src/main/java/com/example/yu05h/osero/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.yu05h.osero;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import java.net.MalformedURLException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout GD = new GridLayout(this);
+        GD = (GridLayout)findViewById(R.id.main_grid);
+
+        int grigNum = 8;
+
+        for (int i=0; i < grigNum; i++) {
+            for (int j = 0; j <grigNum; j++){
+                Button Masu = new Button(this);
+                Masu.setText(i + "." + j);
+                GridLayout.LayoutParams MasuLayoutParams = new GridLayout.LayoutParams();
+                MasuLayoutParams.width = GD.getWidth();
+                System.out.println("GD.getWidth() : " + GD.getWidth());
+                MasuLayoutParams.height = GD.getHeight();
+                System.out.println("GD.getHeight() : " + GD.getHeight());
+                if (Build.VERSION.SDK_INT >= 21) {
+                    System.out.println("IN");
+                    MasuLayoutParams.rowSpec = GridLayout.spec(i, GridLayout.FILL, 1);
+                    MasuLayoutParams.columnSpec = GridLayout.spec(j, GridLayout.FILL, 1);
+                }
+
+            /*
+            MasuLayoutParams.rowSpec = GridLayout.spec(1);
+            MasuLayoutParams.columnSpec =  GridLayout.spec(i);
+            */
+                Masu.setLayoutParams(MasuLayoutParams);
+
+                Masu.setBackgroundResource(R.drawable.frame_style);
+                GD.addView(Masu);
+
+            }
+        }
     }
 }
